d8b76e104fd1d484a68231626012742247360ff4
IndianJanos <nakazawa.jatekfejlesztes@gmail.com>
2025-01-27 18:21:11 +0100

    Axe drop at wood cutter death

--- you_are_the_weapon/scripts/entities/enemy/wood_cutter.gd
+++ you_are_the_weapon/scripts/entities/enemy/wood_cutter.gd
@@ -2,6 +2,6 @@ extends Enemy
 class_name WoodCutter
 
 func _ready() -> void:
-	packed_weapon = preload("res://scenes/weapons/sword.tscn")
+	packed_weapon = preload("res://scenes/weapons/axe.tscn")
 	
 	super._ready()
